940124a2c6c150c105c877de704b757efe894bf3
Muhammad Abdal Rizky <rizky.muhammad425@gmail.com>
2024-11-22 14:13:02 +0800

    updated database schema

--- djangourse.sql
+++ djangourse.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 21, 2024 at 05:48 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:8889
+-- Generation Time: Nov 22, 2024 at 06:10 AM
+-- Server version: 8.0.35
+-- PHP Version: 8.2.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -15,12 +15,12 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `djangourse`
 --
-CREATE DATABASE IF NOT EXISTS `djangourse` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `djangourse` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `djangourse`;
 
 -- --------------------------------------------------------
@@ -31,9 +31,38 @@ USE `djangourse`;
 
 DROP TABLE IF EXISTS `admins`;
 CREATE TABLE `admins` (
-  `id` int(11) NOT NULL,
-  `credential_id` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `credential_id` int NOT NULL,
   `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE `courses` (
+  `id` int NOT NULL,
+  `category_id` int NOT NULL,
+  `instructor_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` int NOT NULL,
+  `level` enum('beginner','intermediate','advanced') NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_categories`
+--
+
+DROP TABLE IF EXISTS `course_categories`;
+CREATE TABLE `course_categories` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -44,11 +73,11 @@ CREATE TABLE `admins` (
 
 DROP TABLE IF EXISTS `credentials`;
 CREATE TABLE `credentials` (
-  `id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `role_id` int NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` date NOT NULL
+  `created_at` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -59,8 +88,8 @@ CREATE TABLE `credentials` (
 
 DROP TABLE IF EXISTS `instructors`;
 CREATE TABLE `instructors` (
-  `id` int(11) NOT NULL,
-  `credential_id` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `credential_id` int NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -72,9 +101,9 @@ CREATE TABLE `instructors` (
 
 DROP TABLE IF EXISTS `password_resets`;
 CREATE TABLE `password_resets` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `email` varchar(100) NOT NULL,
-  `expired_at` int(11) NOT NULL
+  `expired_at` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -85,7 +114,7 @@ CREATE TABLE `password_resets` (
 
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -106,12 +135,27 @@ INSERT INTO `roles` (`id`, `name`) VALUES
 
 DROP TABLE IF EXISTS `students`;
 CREATE TABLE `students` (
-  `id` int(11) NOT NULL,
-  `credential_id` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `credential_id` int NOT NULL,
   `name` varchar(100) NOT NULL,
   `date_of_birth` date DEFAULT NULL,
   `city` varchar(50) DEFAULT NULL,
   `phone_number` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `withdrawal_requests`
+--
+
+DROP TABLE IF EXISTS `withdrawal_requests`;
+CREATE TABLE `withdrawal_requests` (
+  `id` int NOT NULL,
+  `instructor_id` int NOT NULL,
+  `amount` int NOT NULL,
+  `payment_method` enum('dana','paypal') NOT NULL,
+  `status` enum('approved','rejected','pending') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -124,6 +168,20 @@ CREATE TABLE `students` (
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_admins_to_credentials` (`credential_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_courses_to_course_categories` (`category_id`),
+  ADD KEY `fk_courses_to_instructors` (`instructor_id`);
+
+--
+-- Indexes for table `course_categories`
+--
+ALTER TABLE `course_categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `credentials`
@@ -159,6 +217,12 @@ ALTER TABLE `students`
   ADD KEY `fk_students_to_credentials` (`credential_id`);
 
 --
+-- Indexes for table `withdrawal_requests`
+--
+ALTER TABLE `withdrawal_requests`
+  ADD KEY `fk_withdrawal_requests_to_instructors` (`instructor_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -166,37 +230,49 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course_categories`
+--
+ALTER TABLE `course_categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `credentials`
 --
 ALTER TABLE `credentials`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `instructors`
 --
 ALTER TABLE `instructors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `password_resets`
 --
 ALTER TABLE `password_resets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -206,25 +282,38 @@ ALTER TABLE `students`
 -- Constraints for table `admins`
 --
 ALTER TABLE `admins`
-  ADD CONSTRAINT `fk_admins_to_credentials` FOREIGN KEY (`credential_id`) REFERENCES `credentials` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_admins_to_credentials` FOREIGN KEY (`credential_id`) REFERENCES `credentials` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `fk_courses_to_course_categories` FOREIGN KEY (`category_id`) REFERENCES `course_categories` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_courses_to_instructors` FOREIGN KEY (`instructor_id`) REFERENCES `instructors` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `credentials`
 --
 ALTER TABLE `credentials`
-  ADD CONSTRAINT `fk_credentials_to_roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+  ADD CONSTRAINT `fk_credentials_to_roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `instructors`
 --
 ALTER TABLE `instructors`
-  ADD CONSTRAINT `fk_instructors_credentials` FOREIGN KEY (`credential_id`) REFERENCES `credentials` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_instructors_credentials` FOREIGN KEY (`credential_id`) REFERENCES `credentials` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `students`
 --
 ALTER TABLE `students`
-  ADD CONSTRAINT `fk_students_to_credentials` FOREIGN KEY (`credential_id`) REFERENCES `credentials` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_students_to_credentials` FOREIGN KEY (`credential_id`) REFERENCES `credentials` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `withdrawal_requests`
+--
+ALTER TABLE `withdrawal_requests`
+  ADD CONSTRAINT `fk_withdrawal_requests_to_instructors` FOREIGN KEY (`instructor_id`) REFERENCES `instructors` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
