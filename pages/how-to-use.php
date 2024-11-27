3bfaba198f8b620a08f759553ad558e40708b8ae
Muhammad Abdal Rizky <rizky.muhammad425@gmail.com>
2024-11-27 14:54:38 +0800

    removed unused assets

--- pages/how-to-use.php
+++ pages/how-to-use.php
@@ -26,8 +26,9 @@ if (isset($_SESSION["login"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cara Penggunaan Website</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://code.iconify.design/iconify-icon/2.1.0/iconify-icon.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="how-to-use.css">
 </head>
 
