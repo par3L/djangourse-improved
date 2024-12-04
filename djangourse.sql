466495a91e5bad2447fd283b488f25c4f0f9fbca
Muhammad Abdal Rizky <rizky.muhammad425@gmail.com>
2024-12-04 16:48:40 +0800

    updated database schema

--- djangourse.sql
+++ djangourse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 28, 2024 at 01:22 AM
+-- Generation Time: Dec 04, 2024 at 08:47 AM
 -- Server version: 8.0.35
 -- PHP Version: 8.2.20
 
@@ -34,6 +34,7 @@ CREATE TABLE `courses` (
   `id` int NOT NULL,
   `category_id` int NOT NULL,
   `instructor_id` int NOT NULL,
+  `created_at` int NOT NULL,
   `name` varchar(100) NOT NULL,
   `price` int NOT NULL,
   `level` enum('beginner','intermediate','advanced') NOT NULL,
@@ -72,7 +73,24 @@ DROP TABLE IF EXISTS `course_sections`;
 CREATE TABLE `course_sections` (
   `id` int NOT NULL,
   `course_id` int NOT NULL,
-  `title` varchar(100) NOT NULL
+  `title` varchar(100) NOT NULL,
+  `video_link` varchar(255) NOT NULL,
+  `status` enum('finished','not_finished') NOT NULL DEFAULT 'not_finished'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_tools`
+--
+
+DROP TABLE IF EXISTS `course_tools`;
+CREATE TABLE `course_tools` (
+  `id` int NOT NULL,
+  `course_id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `logo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -117,7 +135,9 @@ CREATE TABLE `instructors` (
   `date_of_birth` date DEFAULT NULL,
   `phone_number` varchar(20) DEFAULT NULL,
   `bio` text,
-  `profile_img` varchar(255) DEFAULT NULL
+  `profile_img` varchar(255) DEFAULT NULL,
+  `preferred_withdrawal_method` enum('paypal','dana') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'paypal',
+  `balance` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -197,6 +217,7 @@ DROP TABLE IF EXISTS `withdrawal_requests`;
 CREATE TABLE `withdrawal_requests` (
   `id` int NOT NULL,
   `instructor_id` int NOT NULL,
+  `created_at` int NOT NULL,
   `amount` int NOT NULL,
   `payment_method` enum('dana','paypal') NOT NULL,
   `status` enum('approved','rejected','pending') NOT NULL
@@ -225,6 +246,13 @@ ALTER TABLE `course_categories`
 --
 ALTER TABLE `course_sections`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course_tools`
+--
+ALTER TABLE `course_tools`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_course_tools_to_courses` (`course_id`);
 
 --
 -- Indexes for table `credentials`
@@ -279,6 +307,7 @@ ALTER TABLE `transactions`
 -- Indexes for table `withdrawal_requests`
 --
 ALTER TABLE `withdrawal_requests`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_withdrawal_requests_to_instructors` (`instructor_id`);
 
 --
@@ -301,6 +330,12 @@ ALTER TABLE `course_categories`
 -- AUTO_INCREMENT for table `course_sections`
 --
 ALTER TABLE `course_sections`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course_tools`
+--
+ALTER TABLE `course_tools`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
@@ -346,6 +381,12 @@ ALTER TABLE `transactions`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `withdrawal_requests`
+--
+ALTER TABLE `withdrawal_requests`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -355,6 +396,12 @@ ALTER TABLE `transactions`
 ALTER TABLE `courses`
   ADD CONSTRAINT `fk_courses_to_course_categories` FOREIGN KEY (`category_id`) REFERENCES `course_categories` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `fk_courses_to_instructors` FOREIGN KEY (`instructor_id`) REFERENCES `instructors` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `course_tools`
+--
+ALTER TABLE `course_tools`
+  ADD CONSTRAINT `fk_course_tools_to_courses` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `credentials`
