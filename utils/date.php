d8d40d0593d46c917083c294365703f41429798f
Muhammad Abdal Rizky <rizky.muhammad425@gmail.com>
2024-12-10 19:49:01 +0800

    added function to convert dates to Indonesian format

--- utils/date.php
+++ utils/date.php
@@ -5,3 +5,12 @@ function convertToWita($epoch) {
     $result = date('d-m-Y H.i', $epoch);
     return "$result WITA";
 }
+
+function convertToIndonesianDate($date) {
+    $months = [
+        1 => 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+        'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    ];
+    $explode = explode('-', $date); // Format: YYYY-MM-DD
+    return $explode[2] . ' ' . $months[(int)$explode[1]] . ' ' . $explode[0];
+}
