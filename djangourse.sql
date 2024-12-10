592d37f37b15a08fe47267288b23c449e6658e08
Muhammad Abdal Rizky <rizky.muhammad425@gmail.com>
2024-12-10 20:42:36 +0800

    updated database schema

--- djangourse.sql
+++ djangourse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 04, 2024 at 08:47 AM
+-- Generation Time: Dec 10, 2024 at 12:42 PM
 -- Server version: 8.0.35
 -- PHP Version: 8.2.20
 
@@ -15,12 +15,12 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `djangourse`
 --
-CREATE DATABASE IF NOT EXISTS `djangourse` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `djangourse` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `djangourse`;
 
 -- --------------------------------------------------------
@@ -36,12 +36,13 @@ CREATE TABLE `courses` (
   `instructor_id` int NOT NULL,
   `created_at` int NOT NULL,
   `name` varchar(100) NOT NULL,
+  `subtitle` varchar(100) NOT NULL,
   `price` int NOT NULL,
   `level` enum('beginner','intermediate','advanced') NOT NULL,
   `description` text NOT NULL,
   `thumbnail` varchar(255) DEFAULT NULL,
   `status` enum('Menunggu','Disetujui') NOT NULL DEFAULT 'Menunggu'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -53,7 +54,7 @@ DROP TABLE IF EXISTS `course_categories`;
 CREATE TABLE `course_categories` (
   `id` int NOT NULL,
   `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `course_categories`
@@ -66,17 +67,30 @@ INSERT INTO `course_categories` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `course_sections`
+-- Table structure for table `course_finished_materials`
 --
 
-DROP TABLE IF EXISTS `course_sections`;
-CREATE TABLE `course_sections` (
+DROP TABLE IF EXISTS `course_finished_materials`;
+CREATE TABLE `course_finished_materials` (
+  `id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `course_material_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_materials`
+--
+
+DROP TABLE IF EXISTS `course_materials`;
+CREATE TABLE `course_materials` (
   `id` int NOT NULL,
   `course_id` int NOT NULL,
+  `ordinal` int NOT NULL,
   `title` varchar(100) NOT NULL,
-  `video_link` varchar(255) NOT NULL,
-  `status` enum('finished','not_finished') NOT NULL DEFAULT 'not_finished'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `video_link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -87,11 +101,39 @@ CREATE TABLE `course_sections` (
 DROP TABLE IF EXISTS `course_tools`;
 CREATE TABLE `course_tools` (
   `id` int NOT NULL,
-  `course_id` int NOT NULL,
   `name` varchar(50) NOT NULL,
   `type` varchar(50) NOT NULL,
   `logo` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `course_tools`
+--
+
+INSERT INTO `course_tools` (`id`, `name`, `type`, `logo`) VALUES
+(1, 'Visual Studio Code', 'Code Editor', 'vscode.png'),
+(2, 'Apache', 'Web Server', 'apache.png'),
+(3, 'MySQL', 'Database Management System', 'sql.png'),
+(4, 'Google Chrome', 'Web Browser', 'chrome.png'),
+(5, 'phpMyAdmin', 'Software', 'phpmyadmin.png'),
+(6, 'Flutter', 'SDK', 'flutter.svg'),
+(7, 'Android Studio', 'IDE', 'android-studio.png'),
+(8, 'CodeIgniter', 'Framework', 'codeigniter.png'),
+(9, 'Laravel', 'Framework', 'laravel.png'),
+(10, 'React', 'Library', 'react.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_tool_galleries`
+--
+
+DROP TABLE IF EXISTS `course_tool_galleries`;
+CREATE TABLE `course_tool_galleries` (
+  `id` int NOT NULL,
+  `course_id` int NOT NULL,
+  `tool_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -106,7 +148,21 @@ CREATE TABLE `credentials` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrolled_courses`
+--
+
+DROP TABLE IF EXISTS `enrolled_courses`;
+CREATE TABLE `enrolled_courses` (
+  `id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `course_id` int NOT NULL,
+  `finished_at` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -119,7 +175,7 @@ CREATE TABLE `favourite_courses` (
   `id` int NOT NULL,
   `student_id` int NOT NULL,
   `course_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -136,9 +192,9 @@ CREATE TABLE `instructors` (
   `phone_number` varchar(20) DEFAULT NULL,
   `bio` text,
   `profile_img` varchar(255) DEFAULT NULL,
-  `preferred_withdrawal_method` enum('paypal','dana') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'paypal',
+  `preferred_withdrawal_method` enum('paypal','dana') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'paypal',
   `balance` int NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -151,7 +207,7 @@ CREATE TABLE `password_resets` (
   `id` int NOT NULL,
   `email` varchar(100) NOT NULL,
   `expired_at` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -163,7 +219,7 @@ DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
   `id` int NOT NULL,
   `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `roles`
@@ -189,7 +245,7 @@ CREATE TABLE `students` (
   `city` varchar(50) DEFAULT NULL,
   `phone_number` varchar(20) DEFAULT NULL,
   `coin_balance` int NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -205,7 +261,7 @@ CREATE TABLE `transactions` (
   `price` int NOT NULL,
   `purchase_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `transaction_type` enum('purchase','topup') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -221,7 +277,7 @@ CREATE TABLE `withdrawal_requests` (
   `amount` int NOT NULL,
   `payment_method` enum('dana','paypal') NOT NULL,
   `status` enum('approved','rejected','pending') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Indexes for dumped tables
@@ -242,17 +298,33 @@ ALTER TABLE `course_categories`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `course_sections`
+-- Indexes for table `course_finished_materials`
 --
-ALTER TABLE `course_sections`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `course_finished_materials`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_course_finished_materials_to_students` (`student_id`),
+  ADD KEY `fk_course_finished_materials_to_course_materials` (`course_material_id`);
+
+--
+-- Indexes for table `course_materials`
+--
+ALTER TABLE `course_materials`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_course_materials_to_courses` (`course_id`);
 
 --
 -- Indexes for table `course_tools`
 --
 ALTER TABLE `course_tools`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course_tool_galleries`
+--
+ALTER TABLE `course_tool_galleries`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_course_tools_to_courses` (`course_id`);
+  ADD KEY `fk_course_tool_galleries_to_course_tools` (`tool_id`),
+  ADD KEY `fk_course_tool_galleries_to_courses` (`course_id`);
 
 --
 -- Indexes for table `credentials`
@@ -260,6 +332,14 @@ ALTER TABLE `course_tools`
 ALTER TABLE `credentials`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_credentials_to_roles` (`role_id`);
+
+--
+-- Indexes for table `enrolled_courses`
+--
+ALTER TABLE `enrolled_courses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_enrolled_courses_to_students` (`student_id`),
+  ADD KEY `fk_enrolled_courses_to_courses` (`course_id`);
 
 --
 -- Indexes for table `favourite_courses`
@@ -327,21 +407,39 @@ ALTER TABLE `course_categories`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `course_sections`
+-- AUTO_INCREMENT for table `course_finished_materials`
 --
-ALTER TABLE `course_sections`
+ALTER TABLE `course_finished_materials`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course_materials`
+--
+ALTER TABLE `course_materials`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `course_tools`
 --
 ALTER TABLE `course_tools`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `course_tool_galleries`
+--
+ALTER TABLE `course_tool_galleries`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `credentials`
 --
 ALTER TABLE `credentials`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `enrolled_courses`
+--
+ALTER TABLE `enrolled_courses`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
@@ -398,16 +496,37 @@ ALTER TABLE `courses`
   ADD CONSTRAINT `fk_courses_to_instructors` FOREIGN KEY (`instructor_id`) REFERENCES `instructors` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
--- Constraints for table `course_tools`
+-- Constraints for table `course_finished_materials`
 --
-ALTER TABLE `course_tools`
-  ADD CONSTRAINT `fk_course_tools_to_courses` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `course_finished_materials`
+  ADD CONSTRAINT `fk_course_finished_materials_to_course_materials` FOREIGN KEY (`course_material_id`) REFERENCES `course_materials` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_course_finished_materials_to_students` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `course_materials`
+--
+ALTER TABLE `course_materials`
+  ADD CONSTRAINT `fk_course_materials_to_courses` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `course_tool_galleries`
+--
+ALTER TABLE `course_tool_galleries`
+  ADD CONSTRAINT `fk_course_tool_galleries_to_course_tools` FOREIGN KEY (`tool_id`) REFERENCES `course_tools` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_course_tool_galleries_to_courses` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `credentials`
 --
 ALTER TABLE `credentials`
   ADD CONSTRAINT `fk_credentials_to_roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `enrolled_courses`
+--
+ALTER TABLE `enrolled_courses`
+  ADD CONSTRAINT `fk_enrolled_courses_to_courses` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_enrolled_courses_to_students` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `favourite_courses`
